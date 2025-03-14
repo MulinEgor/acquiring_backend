4a3e69b08d518280e30572666938d27a2bcf9c81
Bopleromn <bopleromn@gmail.com>
2025-03-14 21:55:42 +0300

    dev: Новые миграции

--- alembic/versions/2025_03_14_1851-8e38f9a191ae_seeds.py
+++ alembic/versions/2025_03_14_1851-8e38f9a191ae_seeds.py
@@ -1,8 +1,8 @@
 """seeds
 
-Revision ID: 8065b19d39f4
-Revises: e9d73be9579a
-Create Date: 2025-03-14 18:40:03.900339+00:00
+Revision ID: 8e38f9a191ae
+Revises: 2bbd24611c6c
+Create Date: 2025-03-14 18:51:45.496291+00:00
 
 """
 
@@ -15,8 +15,8 @@ from src.permissions.enums import PermissionEnum
 from src.permissions.models import PermissionModel
 
 # revision identifiers, used by Alembic.
-revision: str = "8065b19d39f4"
-down_revision: Union[str, None] = "e9d73be9579a"
+revision: str = "8e38f9a191ae"
+down_revision: Union[str, None] = "2bbd24611c6c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
