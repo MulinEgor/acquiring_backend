786068b59b9fc32572ce1afa8d3cd1c617e4dfc4
Bopleromn <bopleromn@gmail.com>
2025-03-15 18:48:55 +0300

    dev: Обновил сиды в миграциях

--- alembic/versions/2025_03_15_1211-91b745f5bf2c_seeds.py
+++ alembic/versions/2025_03_15_1211-91b745f5bf2c_seeds.py
@@ -9,12 +9,14 @@ Create Date: 2025-03-15 12:11:27.138880+00:00
 from typing import Sequence, Union
 
 import sqlalchemy as sa
+from sqlalchemy.orm import Session
 
 from alembic import op
 from src.permissions.enums import PermissionEnum
 from src.permissions.models import PermissionModel
 from src.services.hash_service import HashService
 from src.users.models import UserModel
+from src.users_permissions.models import UsersPermissionsModel
 
 # revision identifiers, used by Alembic.
 revision: str = "91b745f5bf2c"
@@ -24,20 +26,31 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    for permission in PermissionEnum:
-        op.execute(
-            sa.insert(PermissionModel).values(
-                name=permission.value,
-            )
-        )
+    session = Session(bind=op.get_bind())
 
-    op.execute(
-        sa.insert(UserModel).values(
-            email="bopleromn@gmail.com",
-            hashed_password=HashService.generate("admin"),
-            is_2fa_enabled=True,
-        )
+    user = UserModel(
+        email="admin@gmail.com",
+        hashed_password=HashService.generate("admin"),
     )
+
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+
+    for permission in PermissionEnum:
+        permission_model = PermissionModel(name=permission.value)
+        session.add(permission_model)
+        session.commit()
+        session.refresh(permission_model)
+
+        user_permission = UsersPermissionsModel(
+            user_id=user.id,
+            permission_id=permission_model.id,
+        )
+        session.add(user_permission)
+        session.commit()
+
+    session.commit()
 
 
 def downgrade() -> None:
