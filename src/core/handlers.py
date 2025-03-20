4b4f21b78c0265a5b1e9f54ee4e9439886226e7a
Bopleromn <bopleromn@gmail.com>
2025-03-20 11:33:36 +0300

    dev: Новый сервер для запуска приложения

--- src/core/handlers.py
+++ src/core/handlers.py
@@ -6,11 +6,13 @@ from loguru import logger
 
 
 async def http_exception_handler(request: Request, exc: HTTPException):
+    """Обработчик HTTP исключений."""
     logger.warning(f"HTTP исключение: {exc.detail}")
     return ORJSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
 
 
 async def exception_handler(request: Request, exc: Exception):
+    """Обработчик обычных исключений."""
     logger.error(f"Исключение: {exc}")
     return ORJSONResponse(
         status_code=500, content={"detail": "Внутренняя ошибка сервера"}
